ae1da9ec06eee96555a49d8835433341e9c85b99
melody-ste <melody_stephan@icloud.com>
2025-12-10 23:11:09 +0100

    css for fluid header trnsition and scrollbar

--- src/components/Interface.jsx
+++ src/components/Interface.jsx
@@ -4,33 +4,31 @@ import Navbar from "./Navbar";
 
 export default function Interface({ headerVisible })
 {
-    const forward = useKeyboardControls((state) => state.forward)
-    const backward = useKeyboardControls((state) => state.backward)
-    const leftward = useKeyboardControls((state) => state.leftward)
-    const rightward = useKeyboardControls((state) => state.rightward)
+  const forward = useKeyboardControls((state) => state.forward)
+  const backward = useKeyboardControls((state) => state.backward)
+  const leftward = useKeyboardControls((state) => state.leftward)
+  const rightward = useKeyboardControls((state) => state.rightward)
 
-    return (
-      <div className="interface">
-        <Navbar />
+  return (
+    <div className="interface">
+      <Navbar />
 
-        {headerVisible && (
-          <header>
-            <h1 className="title">Mélody Stephan</h1>
-            <h2>Frontend Web Developer & 3D Generalist</h2>
-          </header>
-        )}
+      <header className={headerVisible ? "" : "hidden"}>
+        <h1 className="title">Mélody Stephan</h1>
+        <h2>Frontend Web Developer & 3D Generalist</h2>
+      </header>
 
-        <div className="controls">
-          <div className="raw">
-            <div className={`key ${forward ? "active" : ""}`}></div>
-          </div>
-          <div className="raw">
-            <div className={`key ${leftward ? "active" : ""}`}></div>
-            <div className={`key ${backward ? "active" : ""}`}></div>
-            <div className={`key ${rightward ? "active" : ""}`}></div>
-          </div>
+      <div className="controls">
+        <div className="raw">
+          <div className={`key ${forward ? "active" : ""}`}></div>
         </div>
-
+        <div className="raw">
+          <div className={`key ${leftward ? "active" : ""}`}></div>
+          <div className={`key ${backward ? "active" : ""}`}></div>
+          <div className={`key ${rightward ? "active" : ""}`}></div>
+        </div>
       </div>
-    )
+
+    </div>
+  )
 }
